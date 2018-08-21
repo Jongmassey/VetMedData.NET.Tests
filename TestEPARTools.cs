713ad2029f5d58ca33ea67d1d9dadc10b47b512a
Jon Massey <jm4359@bristol.ac.uk>
2018-08-21 17:31:54 +0100

    EPARTools and constructor

---
+++ TestEPARTools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VetMedData.NET.Util;
+
+namespace VetMedData.Tests
+{
+    [TestClass]
+    public class TestEPARTools
+    {
+        [TestMethod]
+        public void TestCtor()
+        {
+            var tools = EPARTools.Get();
+            Assert.IsNotNull(tools);
+        }
+    }
+}
