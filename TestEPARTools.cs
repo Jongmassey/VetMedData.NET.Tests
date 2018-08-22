be0868f0680e9a1d86a03f14ae49c3b52b4dd117
Jon Massey <jm4359@bristol.ac.uk>
2018-08-22 14:36:14 +0100

    Removed google custom search, added direct EMA scrape

--- TestEPARTools.cs
+++ TestEPARTools.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VetMedData.NET.Util;
 
 namespace VetMedData.Tests
@@ -10,10 +7,13 @@ namespace VetMedData.Tests
     public class TestEPARTools
     {
         [TestMethod]
-        public void TestCtor()
+        public void TestGetSearchResults()
         {
-            var tools = EPARTools.Get();
-            Assert.IsNotNull(tools);
+            var res = EPARTools.GetSearchResults("metacam").Result;
+            Assert.IsNotNull(res,"No results returned");
+            Assert.IsTrue(res.Length>0, "Empty results returned");
+            Assert.IsTrue(res[0].Equals("http://www.ema.europa.eu/docs/en_GB/document_library/EPAR_-_Product_Information/veterinary/000033/WC500065777.pdf")
+                ,$"Wrong url returned: {res[0]}");
         }
     }
 }
